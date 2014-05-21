d8585f84f8a6a47afd81bf4f8d8ba3d3ad0aa0d4
Dave McCall <dave@dave-mccall.com>
2014-05-20 20:54:24 -0500

    Initial version

---
+++ jquery.blink.js
@@ -0,0 +1,25 @@
+/*
+* blink
+*
+* Puts the blink tag back on the table.
+*
+* http://www.dave-mccall.com/
+*
+* Usage:
+*  <blink>Awesome</blink>
+*
+* Released under the MIT license.
+* http://www.opensource.org/licenses/mit-license.php
+*/
+
+jQuery(document).ready(function () {
+	$("blink").each(function () {
+		var $tag = $(this);
+		window.setInterval(function () {
+			if ($tag.css("opacity") == 1)
+				$tag.css("opacity", 0);
+			else
+				$tag.css("opacity", 1);
+		}, 500);
+	});
+});
